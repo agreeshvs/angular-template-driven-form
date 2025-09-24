0e8160d9d9e1235c0720399038ef823eaa16e955
agreeshvs <agreesh777@gmail.com>
2025-09-24 11:26:42 +0530

    Working with Radio Buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,13 @@ export class AppComponent {
   title = 'angular-template-driven-form';
   @ViewChild('registrationForm') form: NgForm;
 
+  genders = [
+    {    id: 'check-male',    value: 'male', display: 'Male'  },
+    {    id: 'check-female',    value: 'female', display: 'Female' },
+    {    id: 'check-other',    value: 'other', display: 'Prefer not to say'  },
+  ]
+
   onFormSubmit(){
-    console.log(this.form.value);
+    console.log(this.form);
   }
 }
