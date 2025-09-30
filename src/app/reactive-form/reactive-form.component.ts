0f2b953f0e08f1077a2078c615cf59c8e30ce1bf
agreeshvs <agreesh777@gmail.com>
2025-09-30 15:33:53 +0530

    Introduction to Reactive Forms

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
   templateUrl: './reactive-form.component.html',
   styleUrls: ['./reactive-form.component.css']
 })
-export class ReactiveFormComponent {
+export class ReactiveFormComponent implements OnInit {
+  reactiveForm: FormGroup;
 
+  ngOnInit(): void {
+    this.reactiveForm = new FormGroup({
+      // Define your form controls here
+      firstName: new FormControl(''),
+      lastName: new FormControl(''),
+      email: new FormControl(''),
+      username: new FormControl(''),
+      dob: new FormControl(''),
+      gender: new FormControl('male'),
+      street: new FormControl(''),
+      country: new FormControl('India'),
+      city: new FormControl(''),
+      region: new FormControl(''),
+      postalCode: new FormControl(''),
+      phone: new FormControl('')
+    });
+  }
+
+  onSubmit(){
+    console.log(this.reactiveForm.value);
+  }
 }
