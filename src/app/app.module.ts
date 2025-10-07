19a114c29cd1c9447ad928ac341f05153947570b
agreeshvs <agreesh777@gmail.com>
2025-10-07 18:07:30 +0530

    Adding html and css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TemplateFormComponent } from './template-form/template-form.component';
 import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { CreateTaskComponent } from './dashboard/create-task/create-task.component';
+import { TaskDetailsComponent } from './dashboard/task-details/task-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TemplateFormComponent,
-    ReactiveFormComponent
+    ReactiveFormComponent,
+    DashboardComponent,
+    FooterComponent,
+    HeaderComponent,
+    CreateTaskComponent,
+    TaskDetailsComponent
   ],
   imports: [
     BrowserModule,
